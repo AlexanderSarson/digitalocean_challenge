e1dfc48bc16d0d61e1aeb63cdf521c4eba54de8e
Alexander Sarson <alexander_sarson@outlook.com>
2021-12-11 12:34:49 +0100

    argo deployed

--- infrastructure/__main__.py
+++ infrastructure/__main__.py
@@ -1,28 +1,31 @@
 """A DigitalOcean Python Pulumi program"""
+import base64
 import pulumi
 import pulumi_digitalocean as do
 from pulumi_kubernetes.helm.v3 import Chart, ChartOpts
 import pulumi_kubernetes as k8s
+from pulumi import ResourceOptions
 
 cluster = do.KubernetesCluster("k8s",
             region="fra1",
             version="1.21.5-do.0",
             node_pool=do.KubernetesClusterNodePoolArgs(
-                name="front-end-pool",
+                name="k8s-node-pool-01",
                 size="s-2vcpu-4gb",
                 node_count=2,
             ))
 
+kube_config = pulumi.Output.unsecret(cluster.kube_configs[0]).raw_config
+k8s_provider = k8s.Provider("k8s-provider", kubeconfig=kube_config)
 
-cluster_config = do.get_kubernetes_cluster(cluster.name).kube_configs[0].raw_config
+argocd_namespace = k8s.core.v1.Namespace("argocdNamespace", opts=ResourceOptions(provider=k8s_provider), metadata={"name": "argocd"})
 
-k8s_provider = k8s.Provider("k8s-provider", kube_config=cluster_config)
-
-apache = Chart('apache-chart',
+argocd = Chart('argocd-chart',
     ChartOpts(
-        chart='apache',
-        version='8.3.2',
-        fetch_opts={'repo': 'https://charts.bitnami.com/bitnami'}),
-    pulumi.ResourceOptions(provider=k8s_provider))
+        chart='argo-cd',
+        version='3.28.0',
+        namespace=argocd_namespace.metadata["name"],
+        fetch_opts={'repo': 'https://argoproj.github.io/argo-helm'}),
+    opts=ResourceOptions(provider=k8s_provider))
 
-pulumi.export("kubeconfig",cluster_config)
+pulumi.export("kubeconfig",kube_config)
